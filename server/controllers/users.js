b47e9d29bdc2f20f27f8c05acdbc95af1a5ba490
Clint Losee <clint.losee@gmail.com>
2020-02-18 15:17:16 -0700

    Logout functionality and auth middleware.

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -88,3 +88,8 @@ exports.login = function(req, res, next) {
     });
   })(req, res, next);
 };
+
+exports.logout = function(req, res) {
+  req.logout();
+  return res.json({ status: 'Session destroyed' });
+};
--- server/routes/users.js
+++ server/routes/users.js
@@ -7,5 +7,6 @@ const UsersCtrl = require('../controllers/users');
 router.get('', UsersCtrl.getUsers);
 router.post('/register', UsersCtrl.register);
 router.post('/login', UsersCtrl.login);
+router.post('/logout', UsersCtrl.logout);
 
 module.exports = router;
