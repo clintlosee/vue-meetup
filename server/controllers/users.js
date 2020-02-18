fe49482225a4c76fb50bcc298d07008a1f6a61da
Clint Losee <clint.losee@gmail.com>
2020-02-18 15:01:07 -0700

    Setup for register and login on server.

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,13 +1,90 @@
+const passport = require('passport');
 const User = require('../models/users');
 
 exports.getUsers = function(req, res) {
-  User.find({})
-        .exec((errors, users) => {
-
+  User.find({}).exec((errors, users) => {
     if (errors) {
-      return res.status(422).send({errors});
+      return res.status(422).send({ errors });
     }
 
     return res.json(users);
   });
-}
+};
+
+exports.register = function(req, res) {
+  const registerData = req.body;
+
+  if (!registerData.email) {
+    return res.status(422).json({
+      errors: {
+        email: 'is required',
+      },
+    });
+  }
+
+  if (!registerData.password) {
+    return res.status(422).json({
+      errors: {
+        password: 'is required',
+      },
+    });
+  }
+
+  if (registerData.password !== registerData.passwordConfirmation) {
+    return res.status(422).json({
+      errors: {
+        password: 'do not match',
+      },
+    });
+  }
+
+  const user = new User(registerData);
+
+  user.save((errors, savedUser) => {
+    if (errors) {
+      return res.status(422).json({ errors });
+    }
+    return res.json(savedUser);
+  });
+};
+
+exports.login = function(req, res, next) {
+  const { email, password } = req.body;
+
+  if (!email) {
+    return res.status(422).json({
+      errors: {
+        email: 'is required',
+      },
+    });
+  }
+
+  if (!password) {
+    return res.status(422).json({
+      errors: {
+        password: 'is required',
+      },
+    });
+  }
+
+  return passport.authenticate('local', (err, user) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (!user) {
+      return res.status(422).send({
+        errors: {
+          authentication: 'Ooops, something went wrong!',
+        },
+      });
+    }
+
+    req.login(user, err => {
+      if (err) {
+        next(err);
+      }
+      return res.json(user);
+    });
+  })(req, res, next);
+};
--- server/index.js
+++ server/index.js
@@ -1,27 +1,54 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
+const MongoDBStore = require('connect-mongodb-session')(session);
 const config = require('./config/dev');
 
-require("./models/meetups");
-require("./models/users");
-require("./models/threads");
-require("./models/posts");
-require("./models/categories");
+const store = new MongoDBStore({
+  uri: config.DB_URI,
+  collection: 'meetupSessions',
+});
 
-const meetupsRoutes = require('./routes/meetups'),
-      usersRoutes = require('./routes/users'),
-      threadsRoutes = require('./routes/threads'),
-      postsRoutes = require('./routes/posts'),
-      categoriesRoutes = require('./routes/categories');
+store.on('error', err => console.log(err));
 
-mongoose.connect(config.DB_URI, { useNewUrlParser: true })
+require('./models/meetups');
+require('./models/users');
+require('./models/threads');
+require('./models/posts');
+require('./models/categories');
+
+require('./services/passport');
+
+const meetupsRoutes = require('./routes/meetups');
+const usersRoutes = require('./routes/users');
+const threadsRoutes = require('./routes/threads');
+const postsRoutes = require('./routes/posts');
+const categoriesRoutes = require('./routes/categories');
+
+mongoose.set('useCreateIndex', true);
+mongoose
+  .connect(config.DB_URI, { useNewUrlParser: true })
   .then(() => console.log('DB Connected!'))
   .catch(err => console.log(err));
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use(
+  session({
+    secret: config.SESSION_SECRET,
+    cookie: { maxAge: 3600000 }, // 1hr
+    resave: false,
+    saveUninitialized: false,
+    store,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/api/v1/meetups', meetupsRoutes);
 app.use('/api/v1/users', usersRoutes);
@@ -31,6 +58,6 @@ app.use('/api/v1/categories', categoriesRoutes);
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT , function() {
-  console.log('App is running on port: ' + PORT);
+app.listen(PORT, function() {
+  console.log(`App is running on port: ${PORT}`);
 });
